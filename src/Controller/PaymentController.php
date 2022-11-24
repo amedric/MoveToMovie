84c1661d8a339d00c17d45a208966121fd34f3c5
ToloCrook <tolotraramaro@hotmail.fr>
2022-11-24 18:48:57 +0100

    pushing form, payment and relevant css

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+
+class PaymentController extends AbstractController
+{
+
+    public function successful(): string
+    {
+        return $this->twig->render('Payment/successful.html.twig');
+    }
+
+}
