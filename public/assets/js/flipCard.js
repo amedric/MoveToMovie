4be0f2ace6db34d1968740813284b34adf40f26a
AnthonyMarche <marche.anthony72@gmail.com>
2022-11-25 02:44:44 +0100

    wip

---
+++ public/assets/js/flipCard.js
@@ -0,0 +1,7 @@
+const card = document.querySelectorAll(".card__inner");
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', function () {
+        card[i].classList.toggle('is-flipped');
+    });
+}
